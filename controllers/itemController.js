9094c5b8a415c8d99278ff56b10dfbf5b1bd6d4f
Pixel <petngalan@gmail.com>
2023-07-18 13:04:05 +0200

    Create the controller to handle the get and post request for the form that creates an item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,7 @@
+const { body, validationResult } = require("express-validator");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const Category = require("../models/category");
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("index");
@@ -20,13 +22,56 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: item create GET");
+  const allCategories = await Category.find().exec();
+
+  res.render("item_form", { categories: allCategories });
 });
 
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: item create POST");
-});
+exports.item_create_post = [
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Category must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("stock", "Stock must not be empty").trim().isLength({ min: 1 }).escape(),
 
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().exec();
+
+      // Mark our selected category as checked.
+      for (const category of allCategories) {
+        if (item.category.indexOf(category._id) > -1) {
+          category.checked = "true";
+        }
+      }
+
+      res.render("item_form", {
+        categories: allCategories,
+        item: item,
+        errors: errors,
+      });
+    } else {
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: item delete GET");
 });
